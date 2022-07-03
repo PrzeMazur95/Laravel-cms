3411785c2526a744ec335d51a097049f16ab1e6f
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-07-03 15:48:56 +0200

    Edit redirections

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -35,6 +35,19 @@
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
+
+                @if(Auth::check())
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('admin.index')}}">Admin</a>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Register</a>
+                    </li>
+                @endif
                 <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
                 </li>
