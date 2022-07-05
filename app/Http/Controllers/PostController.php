0a27841153be7872e0b4d1bf2fdcd32fb501e31a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-07-05 19:43:43 +0200

    Add view posts page in Admin side

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function index()
+    {
+
+        return view('admin/posts.index');
+
+
+    }
     public function show(Post $post)
     {
 
