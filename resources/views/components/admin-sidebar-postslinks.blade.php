342c67206d8078e4f4e0333eccb24d94430a3532
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-07-07 19:03:40 +0200

    Fix redirecting error

--- resources/views/components/admin-sidebar-postslinks.blade.php
+++ resources/views/components/admin-sidebar-postslinks.blade.php
@@ -6,8 +6,8 @@
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="{{'post.create'}}">Create a Post</a>
-            <a class="collapse-item" href="{{'post.index'}}">View All Posts</a>
+            <a class="collapse-item" href="{{route('post.create')}}">Create a Post</a>
+            <a class="collapse-item" href="{{route('post.index')}}">View All Posts</a>
         </div>
     </div>
 </li>
