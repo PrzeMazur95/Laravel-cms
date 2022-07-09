ac3cd70f69c799d54627f8ea40870a63d49584e6
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-07-09 08:08:46 +0200

    Add values to edit page of editing post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -6,14 +6,17 @@
             @csrf
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" name="title" class="form-control" id="title" aria-describedby="" placeholder="Enter title">
+                <input type="text" name="title" class="form-control" id="title" value="{{$post->title}} "aria-describedby="" placeholder="Enter title">
+            </div>
+            <div>
+                <img width="300px" src="{{$post->post_image}}" alt="">
             </div>
             <div class="form-group">
                 <label for="file">File</label>
                 <input type="file" name="post_image" class="form-control-file" id="post_image" placeholder="">
             </div>
             <div class="form-group">
-                <textarea name="body" class="form-control" id="body" cols="30" rows="10"></textarea>
+                <textarea name="body" class="form-control" id="body" cols="30" rows="10">{{$post->body}}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
