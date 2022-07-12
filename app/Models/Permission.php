c6fffee0284d90721b3aedcd10a0d828aacfe82e
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-07-12 18:22:42 +0200

    Add quarded, to have all fields fillable

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -9,6 +9,7 @@ class Permission extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
     public function roles(){
 
         return $this->belongsToMany(Role::class);
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,6 +9,7 @@ class Role extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
     public function permissions(){
 
         return $this->belongsToMany(Permission::class);
