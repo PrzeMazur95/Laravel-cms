34af43aaf7c9c3b4f85900318cb69da83268255b
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-07-20 18:16:29 +0200

    Add table to show all users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,5 +1,54 @@
 <x-admin-master>
     @section('content')
-        @endesection
+        <h1>Users</h1>
+    <div class="card shadow mv-4">
+        <div class="card-header py3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="users-table">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Register</th>
+                        <th>Update profile date</th>
+                    </tr>
+                    </thead>
+                    <tfoot>
+                    <tr>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Avatar</th>
+                        <th>Name</th>
+                        <th>Register</th>
+                        <th>Update profile date</th>
+                    </tr>
+                    </tfoot>
+                    <tbody>
+                    @foreach( $users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+        @endsection
 
+        @section('scripts')
+
+            <!-- Page level plugins -->
+            <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
+            <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+            <!-- Page level custom scripts -->
+            <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
+
+        @endsection
 </x-admin-master>
--- resources/views/components/admin-sidebar-userslinks.blade.php
+++ resources/views/components/admin-sidebar-userslinks.blade.php
@@ -6,7 +6,6 @@
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Users</h6>
-            <a class="collapse-item" href="{{route('post.create')}}">Create a Post</a>
             <a class="collapse-item" href="{{route('users.index')}}">View All Users</a>
         </div>
     </div>
